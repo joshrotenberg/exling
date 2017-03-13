c58bdcc7d988ef410f5f4d88317860e63cd034d9
josh rotenberg <joshrotenberg@gmail.com>
2017-03-12 17:13:32 -0700

    most of the basic builder stuff, need actual request execution now

--- lib/exling.ex
+++ lib/exling.ex
@@ -4,45 +4,78 @@ defmodule Exling do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Exling.hello
-      :world
-
+    Returns a new Exling.Request.
   """
-  def hello do
-    :world
-  end
-
-
   def new(), do:
-     %Exling.Request{}
-  
+  %Exling.Request{}
+
+  @doc """
+    Set the base URI (or really as much URI information as you want).
+
+    ## Examples
+      r = Exling.new |> 
+          Exling.base("http://example.com")
+  """
   def base(request, base), do:
-    %{request | raw_url: base}
+  %{request | uri: URI.parse(base)}
+
+  defp set_method_and_uri(request, method, path), do: 
+  %{request | method: method, uri: URI.merge(request.uri, path)}
+
+  @doc """
+    Set the URI path.
+
+    ## Examples
+    r = Exling.new |>
+        Exling.base("http://example.com") |>
+        Exling.path("/some/path")
+  """
+  def path(request, path), do:
+  set_method_and_uri(request, request.method, path)
 
   # methods
-  #
-  def get(request), do:
-    %{request | method: :get}
+  def get(request, path), do:
+  set_method_and_uri(request, :get, path)
 
-  def post(request), do:
-    %{request | method: :post}
+  def post(request, path), do:
+  set_method_and_uri(request, :post, path)
 
-  def delete(request), do:
-    %{request | method: :delete}
+  def delete(request, path), do:
+  set_method_and_uri(request, :delete, path)
 
-  def patch(request), do: 
-    %{request | method: :patch}
+  def patch(request, path), do: 
+  set_method_and_uri(request, :patch, path)
 
-  def head(request), do:
-    %{request | method: :head}
+  def head(request, path), do:
+  set_method_and_uri(request, :head, path)
+
+  def options(request, path), do:
+  set_method_and_uri(request, :options, path)
 
   # headers
   def add(request, k, v) do
-    update_in(request.headers, &List.insert_at(&1, 0, [k,v]))
+    update_in(request.headers, &List.insert_at(&1, 0, {k,v}))
   end
+
+  def set(request, k, v) do
+    headers = case List.keymember?(request.headers, k, 0) do
+      true -> List.keyreplace(request.headers, k, 0, {k,v})
+      _ -> request.headers ++ [{k, v}]
+    end
+    Map.merge(request, %{headers: headers})
+  end
+
+  # body
+  def body(request, body), do:
+  %{request | body: body}
+
+  def body_form(request, body), do:
+  %{request | body: {:form, body}}
+
+  def body_json(request, body), do:
+  %{request | body: Poison.encode!(body)}
+
+  def query(request, query), do:
+  %{request | uri: URI.merge(request.uri, URI.parse("?" <> URI.encode_query(query)))}
 
 end
