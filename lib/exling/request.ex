83a383a04971e3ce8673cca008f3aa349bb9fd5e
josh rotenberg <joshrotenberg@gmail.com>
2017-03-18 15:44:21 -0700

    typespecs

--- lib/exling/request.ex
+++ lib/exling/request.ex
@@ -1,3 +1,4 @@
 defmodule Exling.Request do
-  defstruct uri: nil, method: :get, body: "", headers: []
+  defstruct uri: nil, method: :get, body: "", headers: [], client: :httpoison
+  @type t :: %__MODULE__{uri: %URI{}, method: atom, body: term, headers: list, client: atom }
 end
